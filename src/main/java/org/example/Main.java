585bd59c0d1c129ea66b5329df22d6b160f15bc2
Jhonatan Grueso <jhonper86@gmail.com>
2022-07-05 18:45:35 -0500

    Actualización, tema1 - java básico

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,41 +5,34 @@ public class Main {
 
         //Enteros:
         byte number1 = 1; // 1 byte
-        System.out.println(number1);
         short number2 = 2; // 2 byte
-        System.out.println(number2);
         int number3 = 3; // 4 byte
-        System.out.println(number3);
         long number4 = 4; // 8 byte
-        System.out.println(number4);
+        System.out.println("Numeros enteros, byte: "+ number1 + ", short: " + number2 + ", int: " + number3 + ", long: " + number4);
 
         // Punto Decimal o Flotante:
         float decimal1 = 4.9f;
-        System.out.println(decimal1);
         double decimal2 = 9.99d;
-        System.out.println(decimal2);
+        System.out.println("Numero flotante: " + decimal1 + ", Numero decimales: " + decimal2);
 
         // Carater:
         char caracter1 = 'a';
-        System.out.println(caracter1);
+        System.out.println("Tipo caracter: " + caracter1);
 
         // Booleanos
         boolean verdadero = true;
-        System.out.println(verdadero);
         boolean falso = false;
-        System.out.println(falso);
+        System.out.println("Booleanos: " + verdadero + " o " + falso);
 
         // Cadenas de texto
         String nombre = "Alan";
-        System.out.println(nombre);
         String apellido = "Sastre";
-        System.out.println(apellido);
+        System.out.println("Cadenas de texto, String: " + nombre + " " +apellido);
 
         // Tipos Envoltorios
         Integer numero = null; //Utilizado más para bases de datos.
-        System.out.println(numero);
         long numero4 = 4L;
-        System.out.println(numero4);
+        System.out.println("Numero tipo envoltorio, Integer: " + numero + ", numero long: " + numero4);
 
     }
 }
